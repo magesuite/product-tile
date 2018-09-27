f80fe66bd3d3376afe15953e85c722e2610e6ef8
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2018-09-27 13:02:45 +0200

    Added reviews rating to tile

---
+++ Block/Tile/Fragment/Review.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace MageSuite\ProductTile\Block\Tile\Fragment;
+
+class Review implements \Magento\Framework\View\Element\Block\ArgumentInterface
+{
+    /**
+     * @var \Magento\Review\Model\Review
+     */
+    protected $review;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
+    public function __construct(
+        \Magento\Review\Model\Review $review,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
+    )
+    {
+        $this->review = $review;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * @param \Magento\Catalog\Model\Product $product
+     * @return float
+     */
+    public function getActiveStars(\Magento\Catalog\Model\Product $product)
+    {
+        $ratingSummary = $this->getRatingSummary($product);
+
+        $activeStars = $ratingSummary->getRatingSummary() ? (round($ratingSummary->getRatingSummary() / 10) / 2) : 0;
+
+        return $activeStars;
+    }
+
+    /**
+     * @param \Magento\Catalog\Model\Product $product
+     * @return int
+     */
+    public function getReviewsCount(\Magento\Catalog\Model\Product $product)
+    {
+        $ratingSummary = $this->getRatingSummary($product);
+
+        return $ratingSummary->getReviewsCount();
+    }
+
+    protected function getRatingSummary(\Magento\Catalog\Model\Product $product)
+    {
+        $storeId = $this->storeManager->getStore()->getId();
+
+        $ratingSummary = $product->getRatingSummary();
+
+        if (!$ratingSummary) {
+            $this->review->getEntitySummary($product, $storeId);
+        }
+
+        return $product->getRatingSummary();
+    }
+}
