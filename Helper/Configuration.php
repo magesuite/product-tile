1285bf72a7bab394c274f5f415ee7317ed543601
Hanna Ignatowicz <hanna.ignatowicz@creativestyle.pl>
2025-09-19 17:24:12 +0200

    feat: [HAAR-4911] Limit image cache key to configurable products, add options sorting to minimize uniqueness

--- Helper/Configuration.php
+++ Helper/Configuration.php
@@ -6,10 +6,11 @@ namespace MageSuite\ProductTile\Helper;
 
 class Configuration extends \Magento\Framework\App\Helper\AbstractHelper
 {
-    const XML_PATH_THUMBNAIL_GALLERY_ENABLED = 'product_tile/thumbnail_gallery/enabled';
-    const XML_PATH_INCLUDE_CUSTOMER_GROUP_IN_CACHE_KEY = 'product_tile/cache/include_customer_group_in_cache_key';
+    public const XML_PATH_THUMBNAIL_GALLERY_ENABLED = 'product_tile/thumbnail_gallery/enabled';
+    public const XML_PATH_INCLUDE_CUSTOMER_GROUP_IN_CACHE_KEY = 'product_tile/cache/include_customer_group_in_cache_key';
+    public const XML_PATH_LIMIT_IMAGE_VARIATIONS_IN_CACHE_KEY = 'product_tile/cache/limit_image_variations_in_cache_key';
 
-    public function isThumbnailGalleryEnabled($storeId = null): bool
+    public function isThumbnailGalleryEnabled(mixed $storeId = null): bool
     {
         return $this->scopeConfig->isSetFlag(
             self::XML_PATH_THUMBNAIL_GALLERY_ENABLED,
@@ -18,10 +19,19 @@ class Configuration extends \Magento\Framework\App\Helper\AbstractHelper
         );
     }
 
-    public function includeCustomerGroupInCacheKey($storeId = null) : int
+    public function includeCustomerGroupInCacheKey(mixed $storeId = null): int
     {
         return (int)$this->scopeConfig->getValue(
             self::XML_PATH_INCLUDE_CUSTOMER_GROUP_IN_CACHE_KEY,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+
+    public function isCacheKeyEnabledForVariantImage(mixed $storeId = null): bool
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::XML_PATH_LIMIT_IMAGE_VARIATIONS_IN_CACHE_KEY,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
             $storeId
         );
