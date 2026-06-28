f3399cc7305060fc05d4d56d885f00bbdbe811c9
Zbigniew Kuraś <zbigniew.kuras@creativestyle.pl>
2026-06-28 21:55:24 +0200

    feat: MGS-5401 Fix update qty wishlist

--- Cache/WishlistItem.php
+++ Cache/WishlistItem.php
@@ -13,7 +13,7 @@ class WishlistItem implements CacheKeyModel, \Magento\Framework\View\Element\Blo
         /** @var \Magento\Wishlist\Model\Item $wishlistItem */
         $wishlistItem = $fragment->getTile()->getWishlistItem();
 
-        if(!$wishlistItem) {
+        if (!$wishlistItem) {
             return [];
         }
 
